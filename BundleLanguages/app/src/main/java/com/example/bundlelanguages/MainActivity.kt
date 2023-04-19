616112788d94ccabed8d3d0941d25fe952b8b455
VASS\tomas.macri <tomas.macri@vass.es>
2023-04-19 17:28:39 +0200

    Implement ViewBinding

--- BundleLanguages/app/src/main/java/com/example/bundlelanguages/MainActivity.kt
+++ BundleLanguages/app/src/main/java/com/example/bundlelanguages/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.bundlelanguages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bundlelanguages.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _binding: ActivityMainBinding
+    val binding get() = _binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
