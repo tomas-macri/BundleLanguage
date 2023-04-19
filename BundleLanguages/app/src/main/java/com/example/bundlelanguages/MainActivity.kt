0f0ab0462e1708f65fe67c3db63b4c6fff6e9eaf
VASS\tomas.macri <tomas.macri@vass.es>
2023-04-19 17:33:10 +0200

    Apply a simple adapter to the spinner to show options

--- BundleLanguages/app/src/main/java/com/example/bundlelanguages/MainActivity.kt
+++ BundleLanguages/app/src/main/java/com/example/bundlelanguages/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.bundlelanguages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.bundlelanguages.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.spinnerLanguages.adapter = ArrayAdapter(
+            this,
+            com.google.android.material.R.layout.support_simple_spinner_dropdown_item,
+            listOf("AAAA", "BBBBB", "FFFFF")
+        )
+
     }
 }
