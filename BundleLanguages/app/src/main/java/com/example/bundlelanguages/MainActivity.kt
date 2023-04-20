c1fc5af27bd9b92c25ec4dd45d6a9b59fe7afb51
Jorge Martín Llorente <jorge.martin@vass.es>
2023-04-20 11:46:08 +0200

    Display the list of installed locales in the spinner and filter out the ones that are not in the app using updateConfiguration() even though it is deprecated

--- BundleLanguages/app/src/main/java/com/example/bundlelanguages/MainActivity.kt
+++ BundleLanguages/app/src/main/java/com/example/bundlelanguages/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.bundlelanguages
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.LocaleList
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -12,10 +13,15 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     private lateinit var _binding: ActivityMainBinding
     val binding get() = _binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // TODO: This is a list of languages available in the app
+        //  this should be generated automatically or stored in a better place
+        val appLanguages = mutableListOf("en_US","es_US")
 
         //Get installed locales into a list
         val installedLocales: MutableList<Locale> = mutableListOf()
@@ -23,26 +29,38 @@ class MainActivity : AppCompatActivity() {
             installedLocales.add(resources.configuration.locales.get(i))
         }
 
+        // Display the list of installed locales in the spinner
+        //  and filter out the ones that are not in the app
         binding.spinnerLanguages.adapter = ArrayAdapter(
             this,
             com.google.android.material.R.layout.support_simple_spinner_dropdown_item,
-            installedLocales.map { it.displayName }
+            installedLocales.filter { it.toString() in appLanguages }.map { it.displayName }
         )
+
 
         binding.spinnerLanguages.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val locale = installedLocales[position]
 
-                if (locale == resources.configuration.locales.get(0)) {
+                //If the selected locale is already the first one, do nothing
+                if (position == 0) {
                     return
                 }
 
+                //Move the selected locale to the first position
+                val locale = installedLocales[position]
+                installedLocales.remove(locale)
+                installedLocales.add(0, locale)
+
+                //Update the configuration and recreate the activity
                 val config = Configuration(resources.configuration)
-                // TODO: This should be setLocales and a LocaleList, with the first locale being the one you want to display
-                config.setLocale(locale)
-                // TODO: This is deprecated, but I don't know what to use instead
-                resources.configuration.setTo(config)
-                resources.displayMetrics.setTo(resources.displayMetrics)
+                val localeList = LocaleList(*installedLocales.toTypedArray())
+                config.setLocales(localeList)
+
+                // TODO: Avoid deprecated method
+                resources.updateConfiguration(config, resources.displayMetrics)
+
+                //Set the spinner to the first position
+                binding.spinnerLanguages.setSelection(0)
                 recreate()
             }
 
