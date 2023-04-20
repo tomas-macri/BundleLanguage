fdd884f57b3f1935c625ba247fd64ee18d1882f4
Jorge Martín Llorente <jorge.martin@vass.es>
2023-04-20 08:36:15 +0200

    load installed locales, changing locale when choosing one

--- BundleLanguages/app/src/main/java/com/example/bundlelanguages/MainActivity.kt
+++ BundleLanguages/app/src/main/java/com/example/bundlelanguages/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.bundlelanguages
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import com.example.bundlelanguages.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var _binding: ActivityMainBinding
@@ -13,11 +17,38 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Get installed locales into a list
+        val installedLocales: MutableList<Locale> = mutableListOf()
+        for (i in 0 until resources.configuration.locales.size()) {
+            installedLocales.add(resources.configuration.locales.get(i))
+        }
+
         binding.spinnerLanguages.adapter = ArrayAdapter(
             this,
             com.google.android.material.R.layout.support_simple_spinner_dropdown_item,
-            listOf("AAAA", "BBBBB", "FFFFF")
+            installedLocales.map { it.displayName }
         )
+
+        binding.spinnerLanguages.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val locale = installedLocales[position]
+
+                if (locale == resources.configuration.locales.get(0)) {
+                    return
+                }
+
+                val config = Configuration(resources.configuration)
+                // TODO: This should be setLocales and a LocaleList, with the first locale being the one you want to display
+                config.setLocale(locale)
+                // TODO: This is deprecated, but I don't know what to use instead
+                resources.updateConfiguration(config, resources.displayMetrics)
+                recreate()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //Do nothing
+            }
+        }
 
     }
 }
